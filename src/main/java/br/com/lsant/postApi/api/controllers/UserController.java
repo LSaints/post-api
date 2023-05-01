b237e292ff20219ad959de948f1c1fc09722ceb0
LSaints <mateus.dev@gmail.com>
2023-04-30 23:37:38 -0300

    create controller for user

---
+++ src/main/java/br/com/lsant/postApi/api/controllers/UserController.java
@@ -0,0 +1,52 @@
+package br.com.lsant.postApi.api.controllers;
+
+import br.com.lsant.postApi.api.mappers.UserMapper;
+import br.com.lsant.postApi.api.requests.UserRequestBody;
+import br.com.lsant.postApi.domain.models.User;
+import br.com.lsant.postApi.domain.services.UserServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserServices services;
+    private final UserMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<List<UserRequestBody>> findAll() {
+        return new ResponseEntity<>(mapper.toUserRequestBodyList(services.findAll()), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserRequestBody> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(mapper.toUserRequestBody(services.findById(id)), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody UserRequestBody userRequestBody) {
+        return new ResponseEntity<>(services.save(mapper.toUser(userRequestBody)), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@RequestBody UserRequestBody userRequestBody, @PathVariable Long id) {
+        userRequestBody.setId(id);
+        userRequestBody.setDateUpdate(new Date());
+        return new ResponseEntity<>(services.update(mapper.toUser(userRequestBody)), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        UserRequestBody userRequestBody = mapper.toUserRequestBody(services.findById(id));
+        services.delete(userRequestBody.getId());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
