b4392056c6f02c6eea18536196639123113d5139
LSaints <mateus.dev@gmail.com>
2023-05-01 15:03:16 -0300

    create post mapper

---
+++ src/main/java/br/com/lsant/postApi/data/mappers/CommentMapper.java
@@ -0,0 +1,17 @@
+package br.com.lsant.postApi.data.mappers;
+
+import br.com.lsant.postApi.data.requests.CommentRequestBody;
+import br.com.lsant.postApi.domain.models.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+
+    CommentRequestBody toCommentBodyRequst(Comment comment);
+    List<CommentRequestBody> toCommentRequestBodyList(Comment comment);
+    Comment toComment(CommentRequestBody commentRequestBody);
+}
