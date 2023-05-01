6cff05e9862beb3ef20d6f2fa775c9a6e3e2f34c
LSaints <mateus.dev@gmail.com>
2023-05-01 13:08:02 -0300

    create post services

---
+++ src/main/java/br/com/lsant/postApi/domain/services/PostServices.java
@@ -0,0 +1,47 @@
+package br.com.lsant.postApi.domain.services;
+
+import br.com.lsant.postApi.domain.models.Post;
+import br.com.lsant.postApi.domain.repositories.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServices {
+    private final PostRepository repository;
+
+    public List<Post> findAll() {
+        return repository.findAll();
+    }
+
+    public Post findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException(
+                "Post not found ID:" + id + ", Type: " + Post.class.getName()
+        ));
+    }
+
+    public Post save(Post post) {
+        post.setDateIssue(new Date());
+        return repository.save(post);
+    }
+
+    public Post update(Post post) {
+        Post updatedPost = findById(post.getId());
+        updatedPost.setDateUpdate(new Date());
+        updatedPost.setTitle(post.getTitle());
+        updatedPost.setContent(post.getContent());
+        return updatedPost;
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot delete this entity in application");
+        }
+    }
+
+}
