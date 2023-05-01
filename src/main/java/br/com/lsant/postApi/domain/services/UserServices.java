71d825e8442285fa6756f85742645846fdbedcd7
LSaints <mateus.dev@gmail.com>
2023-04-30 21:44:54 -0300

    create Services for user

---
+++ src/main/java/br/com/lsant/postApi/domain/services/UserServices.java
@@ -0,0 +1,47 @@
+package br.com.lsant.postApi.domain.services;
+
+import br.com.lsant.postApi.domain.models.User;
+import br.com.lsant.postApi.domain.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServices {
+    private final UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException(
+                "User not found ID:" + id + "Type: " + User.class.getName()
+        ));
+    }
+
+    public User save(User user) {
+        user.setDateIssue(new Date());
+        return repository.save(user);
+    }
+
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        newUser.setDateIssue(user.getDateIssue());
+        newUser.setDateUpdate(new Date());
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(user.getPassword());
+        return repository.save(newUser);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot delete this entity in application");
+        }
+    }
+}
