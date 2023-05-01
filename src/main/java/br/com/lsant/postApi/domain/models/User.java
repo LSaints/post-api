aafe2394976b9031493b43757b07db2f7c1f61de
LSaints <mateus.dev@gmail.com>
2023-04-30 21:44:33 -0300

    implements lombok annotation @Data

--- src/main/java/br/com/lsant/postApi/domain/models/User.java
+++ src/main/java/br/com/lsant/postApi/domain/models/User.java
@@ -3,10 +3,12 @@ package br.com.lsant.postApi.domain.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Data;
 
 import static br.com.lsant.postApi.domain.models.User.TABLE_NAME;
 
 @Entity(name = TABLE_NAME)
+@Data
 public class User extends AbstractEntity {
     public static final String TABLE_NAME = "user";
 
