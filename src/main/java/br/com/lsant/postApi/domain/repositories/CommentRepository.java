c615185a010e86f9e6aef6fed15f4c7fdb49ad6f
LSaints <mateus.dev@gmail.com>
2023-05-01 21:03:33 -0300

    add news methods to make query in database

--- src/main/java/br/com/lsant/postApi/domain/repositories/CommentRepository.java
+++ src/main/java/br/com/lsant/postApi/domain/repositories/CommentRepository.java
@@ -4,6 +4,10 @@ import br.com.lsant.postApi.domain.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByAuthorId(Long id);
+    List<Comment> findByPostId(Long id);
 }
