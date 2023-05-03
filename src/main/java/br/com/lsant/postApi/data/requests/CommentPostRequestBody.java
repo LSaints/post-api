29b7563fd145443dda5293c4e0dec67263e2c6a6
LSaints <mateus.dev@gmail.com>
2023-05-03 10:28:25 -0300

    refactor rename class name

--- src/main/java/br/com/lsant/postApi/data/requests/CommentPostRequestBody.java
+++ src/main/java/br/com/lsant/postApi/data/requests/CommentPostRequestBody.java
@@ -1,14 +1,15 @@
 package br.com.lsant.postApi.data.requests;
 
 import br.com.lsant.postApi.domain.models.AbstractEntity;
+import br.com.lsant.postApi.domain.models.Post;
 import br.com.lsant.postApi.domain.models.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class PostRequestBody extends AbstractEntity {
-    private String title;
+public class CommentPostRequestBody extends AbstractEntity {
     private String content;
+    private Post post;
     private User author;
 }
--- src/main/java/br/com/lsant/postApi/data/requests/PostPostRequestBody.java
+++ src/main/java/br/com/lsant/postApi/data/requests/PostPostRequestBody.java
@@ -1,15 +1,14 @@
 package br.com.lsant.postApi.data.requests;
 
 import br.com.lsant.postApi.domain.models.AbstractEntity;
-import br.com.lsant.postApi.domain.models.Post;
 import br.com.lsant.postApi.domain.models.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class CommentRequestBody extends AbstractEntity {
+public class PostPostRequestBody extends AbstractEntity {
+    private String title;
     private String content;
-    private Post post;
     private User author;
 }
