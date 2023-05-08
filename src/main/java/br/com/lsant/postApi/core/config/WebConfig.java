47c2e78355be4a8eb999f815244fa8c018d2d810
LSaints <mateus.dev@gmail.com>
2023-05-08 10:45:20 -0300

    add web configuration file and add cors acess

---
+++ src/main/java/br/com/lsant/postApi/core/config/WebConfig.java
@@ -0,0 +1,14 @@
+package br.com.lsant.postApi.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
