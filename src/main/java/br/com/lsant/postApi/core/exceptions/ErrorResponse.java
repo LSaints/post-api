c6f1e5add5972eb8f399c33c96db5e001855e3bf
LSaints <mateus.dev@gmail.com>
2023-05-08 11:02:49 -0300

    reafactor code

--- src/main/java/br/com/lsant/postApi/core/exceptions/ErrorResponse.java
+++ src/main/java/br/com/lsant/postApi/core/exceptions/ErrorResponse.java
@@ -15,7 +15,6 @@ public class ErrorResponse implements Serializable {
     private String message;
     private String path;
 
-    public ErrorResponse() {
-    }
+    public ErrorResponse() {}
 
 }
